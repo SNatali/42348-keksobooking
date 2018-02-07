4374b5c33fe24718346079b58e3d37952b3b3681
Natalia Sergieieva <bossovivh@mail.ru>
2018-02-07 20:03:00 +0100

    task3

--- js/map.js
+++ js/map.js
@@ -4,16 +4,20 @@ var MIN_ROOMS = 1; // минимальное количество комнат
 var MAX_ROOMS = 5; // максимальное количество комнат
 var MIN_PRICE = 1000; // минимальная стоимость
 var MAX_PRICE = 1000000; // максимальная стоимость
-
-var titile = ['Большая уютная квартира',
-              'Маленькая неуютная квартира',
-              'Огромный прекрасный дворец',
-              'Маленький ужасный дворец',
-              'Красивый гостевой домик',
-              'Некрасивый негостеприимный домик',
-              'Уютное бунгало далеко от моря',
-              'Неуютное бунгало по колено в воде'];
-
+var titles = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря'
+];
+var typeOfRooms = ['flat', 'house', 'bungalo'];
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var checkin = ['12:00', '13:00', '14:00'];
+var checkout = ['12:00', '13:00', '14:00'];
+var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var LOCATION = { // координаты метки
   x: {
     min: 300,
@@ -24,68 +28,75 @@ var LOCATION = { // координаты метки
     max: 500
   }
 };
+var OFFERS_QUANTITY = 8;
 
 var getRandomValue = function (min, max) { // поиск случайного числа
   return Math.floor(Math.random() * (max - min));
 };
 
-var getPicture = function () {
-  for (var i = 0; i <= 8; i++ ) {
-        var count = i + 1;
-        var imgSrc = 'img/avatars/user0' + count + '.png';
-        return imgSrc;
-  }
-};
+var generateObject = function (i) {
+  var locationX = getRandomValue(LOCATION.x.min, LOCATION.x.max); // случайная координата метки по Х
+  var locationY = getRandomValue(LOCATION.y.min, LOCATION.y.max); // случайная координата метки по Y
 
-var locationX = getRandomValue(LOCATION.x.min, LOCATION.x.max); // случайная координата метки по Х
-var locationY = getRandomValue(LOCATION.y.min, LOCATION.y.max); // случайная координата метки по Y
+  return {
+    author: {
+      avatar: 'img/avatars/user0' + (i + 1) + '.png'
+    },
+    offer: {
+      title: titles[i],
+      address: + locationX + ', ' + locationY ,
+      price: getRandomValue(MIN_PRICE, MAX_PRICE),
+      type: typeOfRooms[getRandomValue(0, typeOfRooms.length)],
+      rooms: getRandomValue(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomValue(1, 5),
+      checkin: checkin[getRandomValue(0, checkin.length)],
+      checkout: checkout[getRandomValue(0, checkout.length)],
+      feature: features[getRandomValue(0, features.length)],
+      description: '',
+      photos: photos[getRandomValue(0, photos.length)]
 
-var getTitle = function (names) { // функция перебора случайного заголовка из массива
-  var
-};
-
-var objMap = [{
-  'autor' : {
-    'avatar' : getPicture()
+    },
+    location: {
+      x: locationX,
+      y: locationY
     }
-  },
+  };
+};
 
-  'offer' : {
-    'title' : ['Большая уютная квартира',
-              'Маленькая неуютная квартира',
-              'Огромный прекрасный дворец',
-              'Маленький ужасный дворец',
-              'Красивый гостевой домик',
-              'Некрасивый негостеприимный домик',
-              'Уютное бунгало далеко от моря',
-              'Неуютное бунгало по колено в воде'] ,
 
-    'address' : + locationX + ', ' + locationY ,
-
-    'price' : getRandomValue(MIN_PRICE, MAX_PRICE),//число, случайная цена от 1000 до 1 000 000
-
-    'type' : ['flat', 'house', 'bungalo'];
-
-    'rooms' : getRandomValue(MIN_ROOMS, MAX_ROOMS), //число, случайное количество комнат от 1 до 5
-
-    'guests' : getRandomValue(1, 5), //число, случайное количество гостей, которое можно разместить
-
-    'checkin' : ['12:00', '13:00', '14:00'],
-
-    'checkout' : ['12:00', '13:00', '14:00'],
-
-    'features' : ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-
-    'description' : '',
-
-    'photos' : ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-
-  },
-
-    "location": {
-    'x': locationX,//случайное число, координата x метки на карте от 300 до 900,
-    'y': locationY //случайное число, координата y метки на карте от 150 до 500
+var getOffers = function () { //генерируем массив
+  var data = [];
+  for (var i = 0; i <= OFFERS_QUANTITY; i++) {
+    data.push(generateObject(i));
   }
+  return data;
+};
+//console.log(getOffers());
 
-}];
+var getMap = function (offer) {
+  var element = document.querySelector('.map__pins');
+  var fragment = document.createDocumentFragment();
+  for (var i = 0 ; i < offer.length; i++) {
+    fragment.appendChild(offer[i]);
+  }
+  element.appendChild(fragment);
+};
+
+var map = document.querySelector('template').querySelector('.map__card');
+var titleH4 = map.querySelector('h4');
+
+map.querySelector('h3').offer.title;
+map.querySelector('h3').offer.address;
+map.querySelector('.popup__price').offer.price;
+titleH4.offer.type;
+titleH4.offer.rooms;
+titleH4.offer.guests;
+titleH4.offer.checkin;
+titleH4.offer.checkout;
+map.querySelector('.popup__features').offer.features;
+map.querySelector('.popup__features').offer.description;
+map.querySelector('.popup__pictures').offer.photos;
+
+
+
 
